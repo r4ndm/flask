91d59c058be538caee9fc638702e98ffe7fc9dad
randm- <muuher@yahoo.com>
2016-09-13 17:07:03 -0500

    changes for flask_restplus

--- controllers.py
+++ controllers.py
@@ -1,6 +1,8 @@
-
+from flask_restplus import Resource
 from main import app
+from main import api
 
-@app.route('/index')
-def indexcontroller():
-    return "Server index page"
+@api.route('/index')
+class IndexController(Resource):
+    def get(self):
+        return "Server index page"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_restplus import Api
 
 app = Flask(__name__)
+api = Api(app)
 
 from controllers import *
 
